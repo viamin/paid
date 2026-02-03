318ce8fa29cc7168e6b3c765b8bea00261ad86e9
Bart Agapinan <bart@sonic.net>
2026-02-03 17:18:05 +0000

    fix: update require statements for temporalio dependencies in activities and workflows

--- app/temporal/activities/base_activity.rb
+++ app/temporal/activities/base_activity.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-require "temporalio"
+require "temporalio/activity"
 
 module Activities
   class BaseActivity
--- config/initializers/temporal.rb
+++ config/initializers/temporal.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-require "temporalio"
+require "temporalio/client"
 
 module Paid
   class << self
