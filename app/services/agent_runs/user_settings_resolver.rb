039a45b2332cbad73e6dec46f34f268e713e50ff
Bart Agapinan <bart@sonic.net>
2026-03-06 02:58:26 +0000

    Address latest review feedback on resolver and rate-limit parsing

--- app/services/agent_runs/user_settings_resolver.rb
+++ app/services/agent_runs/user_settings_resolver.rb
@@ -2,6 +2,8 @@
 
 module AgentRuns
   class UserSettingsResolver
+    class MissingUserError < StandardError; end
+
     def self.call(project:, strict: true)
       account = project.account
 
@@ -9,12 +11,7 @@ module AgentRuns
       user ||= account.account_memberships.find_by(role: :owner)&.user
       user ||= account.users.first
 
-      if user.nil? && strict
-        raise Temporalio::Error::ApplicationError.new(
-          "No user available for agent run settings",
-          type: "MissingUser"
-        )
-      end
+      raise MissingUserError, "No user available for project #{project.id}" if user.nil? && strict
 
       user&.settings
     end
