2095958d6b69954c0fcebb17801dec6362076a57
Bart Agapinan <bart@sonic.net>
2026-03-19 13:24:34 +0000

    fix(devcontainer): allow tailscale hosts by default in development

---
+++ lib/paid/tailscale_hosts.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+require "ipaddr"
+
+module Paid
+  module TailscaleHosts
+    HOSTNAME_PATTERN = /\A[a-z0-9-]+\.ts\.net\z/
+    CGNAT_RANGE = IPAddr.new("100.64.0.0/10")
+    FALSE_VALUES = %w[0 false off no].freeze
+
+    module_function
+
+    def enabled?
+      value = ENV["ALLOW_TAILSCALE_HOSTS"]
+      return true if value.nil?
+
+      !FALSE_VALUES.include?(value.strip.downcase)
+    end
+  end
+end
