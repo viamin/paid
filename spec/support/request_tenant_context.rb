68d38e99cc7a1caabde223b09f8e92c259a48202
Bart Agapinan <bart@sonic.net>
2026-04-30 05:09:07 +0000

    fix(tests): skip tenant reset when db is unavailable

--- spec/support/request_tenant_context.rb
+++ spec/support/request_tenant_context.rb
@@ -6,6 +6,9 @@ module RequestTenantContext
       next if RSpec.current_example.metadata[:tenant_isolation]
 
       TenantContext.apply_system_access!
+    rescue ActiveRecord::ConnectionNotEstablished
+      # Some request specs intentionally simulate a dead database connection.
+      # In that case there is no session state to restore for the next query.
     end
   end
 end
