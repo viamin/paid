9694c52a21849072592929ed1b1aa0cd92a01350
Paid Agent <paid-agents@paid-agents.com>
2026-06-28 16:41:48 +0000

    fix(knowledge): enable rls for change intents

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_06_28_091011) do
+ActiveRecord::Schema[8.1].define(version: 2026_06_28_160548) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "hstore"
   enable_extension "pg_catalog.plpgsql"
