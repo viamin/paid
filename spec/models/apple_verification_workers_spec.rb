a708e450d7fc209c2a86299f18cac598a07285c9
Paid Agent <paid-agents@paid-agents.com>
2026-09-19 21:50:40 +0000

    fix(apple-workers): preserve approved project binding

--- spec/models/apple_verification_workers_spec.rb
+++ spec/models/apple_verification_workers_spec.rb
@@ -61,6 +61,21 @@ RSpec.describe "Apple verification persistence", type: :model do
     expect(revision.errors[:base]).to include("approved workflow binding is immutable")
   end
 
+  it "does not permit an approved workflow revision to move to another project" do # @spec APPLE-WORKER-004
+    project = create(:project)
+    other_project = create(:project, account: project.account)
+    administrator = create(:user, account: project.account)
+    administrator.add_role(:project_admin, project)
+    administrator.add_role(:project_admin, other_project)
+    revision = create(:apple_verification_workflow_revision, project:, account: project.account)
+    revision.approve!(actor: administrator)
+
+    revision.project = other_project
+
+    expect(revision).not_to be_valid
+    expect(revision.errors[:base]).to include("approved workflow binding is immutable")
+  end
+
   it "requires a project administrator to approve a workflow revision" do # @spec APPLE-WORKER-004
     project = create(:project)
     revision = create(:apple_verification_workflow_revision, project:)
